77bd093d9b5891ecc87ecf946533130d7afa4ebe
IvanYAIT <YAkushikia20@st.ithub.ru>
2023-04-25 13:32:29 +0300

    complete

---
+++ ConsoleApp6/ConsoleApp6/Prototype/Prototype.cs
@@ -0,0 +1,18 @@
+﻿namespace ConsoleApp6
+{
+    interface IClonable
+    {
+        string Name { get; set; }
+        IClonable Clone();
+    }
+
+    class ClonableClass : IClonable
+    {
+        public string Name { get; set; }
+
+        public IClonable Clone()
+        {
+            return new ClonableClass() { Name = Name };
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Singleton/Singleton.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp6
+{
+    public class Singleton
+    {
+        private static Singleton instance;
+
+        private Singleton() { }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+                instance = new Singleton();
+            return instance;
+        }
+    }
+}
